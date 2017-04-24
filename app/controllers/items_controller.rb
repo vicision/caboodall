51ec431c62bc5fb6baa72692bb80079900d7298d
vicision <duncan.j.mclachlan@gmail.com>
2017-04-24 19:29:30 -0400

    added new item link to items/show view, refactored items_controller and items/new form to allow creation of new genre

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,24 @@ class ItemsController < ApplicationController
     end
   end
 
-  
+  post '/items' do
+    if params[:title].strip != "" && params[:creator].strip != "" && params[:type].strip != ""
+      @item = current_user.items.create(title: params[:title], creator: params[:creator])
+      if !Type.all.include?(params[:type])
+        Type.create(name: params[:type])
+      else
+        @item.type_id = params[:types]
+
+      end
+
+
+      @item.save
+      redirect to "/items"
+    else
+      redirect to "/new"
+    end
+  end
+
+
 
 end
